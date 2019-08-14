d6a0e115b1af6ea72f49a42b42cabfc99147b143
Gonzalo Diaz <info@ventra.com.co>
2019-08-14 20:10:58 +0100

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+      flash.alert = "User successfully created"
+    else
+      flash.alert = "Error"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    userParams = params.require(:user).permit(:email_address)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_08_14_174858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
