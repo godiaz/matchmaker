881605e85bfabd2f679b9a8c8d4caf7b78b6d1d8
Gonzalo Diaz <info@ventra.com.co>
2019-08-27 11:55:15 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users
 
   get '/thanks', to: 'pages#thanks'
+  get '/privacypolicy', to: 'pages#privacypolicy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
